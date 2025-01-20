9292f1abaa5c737a509fcaaca244588b3cf528e9
Bruno Porciuncula <652969+brunogama@users.noreply.github.com>
2025-01-20 07:30:54 -0300

    chore: remove samples on client

--- Sources/VariadicBoxClient/Utils.swift
+++ Sources/VariadicBoxClient/Utils.swift
@@ -1,10 +1,1 @@
 
-func separator(_ separator: String = "-", _ count: Int = 80) {
-    print((0...count).compactMap { _ in separator }.joined().prefixed(by: "\n").suffixed(by: "\n"))
-}
-
-func describer(_ value: Any) {
-    print("Type: \(type(of: value))")
-
-    dump(value)
-}
